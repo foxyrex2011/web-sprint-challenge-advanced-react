5b1530fc239d6e515594012e2bdd9ac1f25b1c84
Frederick Cortez <frederickcortez66@gmail.com>
2022-07-15 11:36:50 -0500

    finished sprint

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,21 +1,27 @@
 import React from 'react'
+import axios from 'axios'
+
+const initialMessage = ''
+const initialEmail = ''
+const initialSteps = 0
+const initialIndex = 4 
 
 export default class AppClass extends React.Component {
   
   state = {
-    message: '',
-    email: '',
-    steps: 0,
-    index: 4,
+    message: initialMessage,
+    email: initialEmail,
+    steps: initialSteps,
+    index: initialIndex,
     board: [0,1,2,3,4,5,6,7,8],
-    xAxis: 2,
-    yAxis: 2
-  }
+    xCoord: 2,
+    yCoord: 2
+  };
 
   getIndex = (id) => {
     const newBoard = [...this.state.board];
     newBoard[id] = this.state.index;
-
+    
     if (id === "up") {
       if(this.state.index > 2) {
         this.setState({
@@ -98,13 +104,44 @@ export default class AppClass extends React.Component {
       })
     }
   }
-  
+
+  moves = (evt) => {
+    this.getIndex(evt.target.id)
+  }
+
+  onChange = (evt) => {
+    this.setState({...this.state, email: evt.target.value})
+  }
+
+  validateEmail = () => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.state.email))
+    {
+      return (true)
+    }
+      return (false)
+  }
+
+  onSubmit = (evt) => {
+    evt.preventDefault();
+    if(this.state.email === ''){
+      this.setState({...this.state, message: 'Ouch: email is required' })
+    } else if(this.state.email === 'foo@bar.baz'){
+      this.setState({...this.state, message: 'foo@bar.baz failure #71'})
+    } else if(this.validateEmail() === false){
+      this.setState({...this.state, message: 'Ouch: email must be a valid email'})
+    } else {
+    axios.post(`http://localhost:9000/api/result`, { "x": this.state.xCoord, "y": this.state.yCoord, "steps": this.state.steps, "email": this.state.email.trim() })
+    .then(res => this.setState({...this.state, message: res.data.message, email: initialEmail}))
+    .catch(err => console.error(err))
+    }
+  }
+
   render() {
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates ({this.state.xAxis},{this.state.yAxis})</h3>
+          <h3 id="coordinates">Coordinates ({this.state.xCoord},{this.state.yCoord})</h3>
           <h3 id="steps">You moved {this.state.steps} {this.state.steps === 1 ? 'time' : 'times'}</h3>
         </div>
         <div id="grid">
@@ -120,14 +157,14 @@ export default class AppClass extends React.Component {
           <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={this.moves} id="left">LEFT</button>
+          <button onClick={this.moves} id="up">UP</button>
+          <button onClick={this.moves} id="right">RIGHT</button>
+          <button onClick={this.moves} id="down">DOWN</button>
+          <button onClick={this.moves} id="reset">reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
+        <form onSubmit={this.onSubmit}>
+          <input onChange={this.onChange} value={this.state.email} id="email" placeholder="type email" type="email"></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,78 +1,171 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 
-// Suggested initial states
 const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
+const initialIndex = 4 
 
 export default function AppFunctional(props) {
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
 
-  function getXY() {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
+  const [ state, setState ] = useState({
+    message: initialMessage,
+    email: initialEmail,
+    steps: initialSteps,
+    index: initialIndex,
+    board: [0,1,2,3,4,5,6,7,8],
+    xCoord: 2,
+    yCoord: 2
+  });
+  
+  const getIndex = (id) => {
+    const newBoard = [...state.board];
+    newBoard[id] = state.index;
+    
+    if (id === "up") {
+      if(state.index > 2) {
+        setState({
+          ...state,
+          index: state.index - 3,
+          steps: state.steps + 1,
+          yCoord: state.yCoord - 1,
+          board: newBoard,
+          message: ''
+        })
+      } else {
+        setState({
+          ...state,
+          message: "You can't go up",
+          board: newBoard
+        })
+      }
+    } else if (id === "right") {
+      if((state.index + 1) % 3) {
+        setState({
+          ...state,
+          index: state.index + 1,
+          steps: state.steps + 1,
+          xCoord: state.xCoord + 1,
+          board: newBoard,
+          message: ''
+        })
+      } else {
+        setState({
+          ...state,
+          message: "You can't go right",
+          board: newBoard
+        })
+      }
+    } else if (id === "down") {
+      if(state.index < 6) {
+        setState({
+          ...state,
+          index: state.index + 3,
+          steps: state.steps + 1,
+          yCoord: state.yCoord + 1,
+          board: newBoard,
+          message: ''
+        })
+      } else {
+        setState({
+          ...state,
+          message: "You can't go down",
+          board: newBoard
+        })
+      }
+    } else if (id === "left") {
+      if(state.index % 3){
+        setState({
+          ...state,
+          xCoord: state.xCoord - 1,
+          index: state.index - 1,
+          steps: state.steps + 1,
+          board: newBoard,
+          message: ''
+        })
+      } else {
+        setState({
+          ...state,
+          message: "You can't go left",
+          board: newBoard
+        })
+      }
+    } else if (id === "reset") {
+      setState({
+        ...state,
+        message: '',
+        email: '',
+        steps: 0,
+        index: 4,
+        board: [0,1,2,3,4,5,6,7,8],
+        xCoord: 2,
+        yCoord: 2,
+        board: newBoard,
+        message: ''
+      })
+    }
   }
 
-  function getXYMessage() {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
+  const moves = (evt) => {
+    getIndex(evt.target.id)
   }
 
-  function reset() {
-    // Use this helper to reset all states to their initial values.
+  const onChange = (evt) => {
+    setState({...state, email: evt.target.value})
   }
 
-  function getNextIndex(direction) {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
+  const validateEmail = () => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(state.email))
+    {
+      return (true)
+    }
+      return (false)
   }
 
-  function move(evt) {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
-  }
-
-  function onChange(evt) {
-    // You will need this to update the value of the input.
-  }
-
-  function onSubmit(evt) {
-    // Use a POST request to send a payload to the server.
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    if(state.email === ''){
+      setState({...state, message: 'Ouch: email is required' })
+    } else if(state.email === 'foo@bar.baz'){
+      setState({...state, message: 'foo@bar.baz failure #71'})
+    } else if(validateEmail() === false){
+      setState({...state, message: 'Ouch: email must be a valid email'})
+    } else {
+    axios.post(`http://localhost:9000/api/result`, { "x": state.xCoord, "y": state.yCoord, "steps": state.steps, "email": state.email.trim() })
+    .then(res => setState({...state, message: res.data.message, email: ''}))
+    .catch(err => console.error(err))
+    }
   }
 
   return (
     <div id="wrapper" className={props.className}>
-      <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
-      </div>
-      <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
-      </div>
-      <div className="info">
-        <h3 id="message"></h3>
-      </div>
-      <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
-      </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
-      </form>
+        <div className="info">
+          <h3 id="coordinates">Coordinates ({state.xCoord},{state.yCoord})</h3>
+          <h3 id="steps">You moved {state.steps} {state.steps === 1 ? 'time' : 'times'}</h3>
+        </div>
+        <div id="grid">
+          {
+            state.board.map(idx => (
+              <div key={idx} className={`square${idx === state.index ? ' active' : ''}`}>
+                {idx === state.index ? 'B' : null}
+              </div>
+            ))
+          }
+        </div>
+        <div className="info">
+          <h3 id="message">{state.message}</h3>
+        </div>
+        <div id="keypad">
+          <button onClick={moves} id="left">LEFT</button>
+          <button onClick={moves} id="up">UP</button>
+          <button onClick={moves} id="right">RIGHT</button>
+          <button onClick={moves} id="down">DOWN</button>
+          <button onClick={moves} id="reset">reset</button>
+        </div>
+        <form onSubmit={onSubmit}>
+          <input onChange={onChange} value={state.email} id="email" type="email" placeholder="type email"></input>
+          <input id="submit" type="submit"></input>
+        </form>
     </div>
   )
 }
